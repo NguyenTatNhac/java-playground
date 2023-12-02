ad69437b94fbb45a6f3605a73735cac191da14b7
Nhac Tat Nguyen <nhactat.nguyen_ext@zooplus.com>
2023-12-02 21:36:08 +0100

    LeetCode 49: Group Anagrams solution 1

---
+++ src/main/java/com/ntnguyen/leetcode/GroupAnagrams49Solution1.java
@@ -0,0 +1,28 @@
+package com.ntnguyen.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagrams49Solution1 {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> groups = new HashMap<>();
+
+        for (String s : strs) {
+            char[] sorted = s.toCharArray();
+            Arrays.sort(sorted);
+            String sortedString = new String(sorted);
+
+            if (groups.containsKey(sortedString)) {
+                groups.get(sortedString).add(s);
+            } else {
+                groups.put(sortedString, new ArrayList<>(List.of(s)));
+            }
+        }
+
+        return new ArrayList<>(groups.values());
+    }
+}
