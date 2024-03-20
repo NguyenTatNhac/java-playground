de2fc85e682d2356cfc303bb08e4ea590006941d
Nhac Tat Nguyen <nhactat.nguyen_ext@zooplus.com>
2024-03-20 16:04:12 +0100

    LeetCode 155: Min Stack

---
+++ src/main/java/com/ntnguyen/leetcode/MinStack155.java
@@ -0,0 +1,37 @@
+package com.ntnguyen.leetcode;
+
+public class MinStack155 {
+    Node head;
+
+    public void push(int val) {
+        if (head == null) {
+            head = new Node(val, val, null);
+        } else {
+            head = new Node(val, Math.min(val, head.min), head);
+        }
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+
+    static class Node {
+        int val;
+        int min;
+        Node next;
+
+        public Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+}
